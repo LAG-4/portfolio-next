663269b6fa5caaf2261490101606cc92ddf47dfd
LAG-4 <aryangupta4feb@gmail.com>
2025-06-04 03:11:27 +0530

    switched from a to link

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,16 +1,16 @@
 "use client"
 import * as React from "react"
-
+import Link from "next/link"
 import { Button } from "@/components/ui/button"
 export default function NotFound() {
 
-return (
-<>
-<div className="flex flex-col h-screen justify-center items-center">
-<h1 className="text-6xl flex w-full justify-center items-center">404 Page not found</h1>
-<Button asChild variant="outline" className="justify-center items-center mt-10">
-<a href="/">Return Home</a></Button>
-</div>
-</>
-)
+    return (
+        <>
+            <div className="flex flex-col h-screen justify-center items-center">
+                <h1 className="text-6xl flex w-full justify-center items-center">404 Page not found</h1>
+                <Button asChild variant="outline" className="justify-center items-center mt-10">
+                    <Link href="/">Return Home</Link></Button>
+            </div>
+        </>
+    )
 }
